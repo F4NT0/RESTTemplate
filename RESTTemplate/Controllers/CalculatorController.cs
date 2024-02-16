0cc2710f9c96b4b361dbfc36a20abaf5b61ca75f
F4NT0 <fantolaboratorio@hotmail.com>
2024-02-16 14:57:52 -0300

    Colocado Postman Request e Atualizado as requisições.

--- RESTTemplate/Controllers/CalculatorController.cs
+++ RESTTemplate/Controllers/CalculatorController.cs
@@ -13,8 +13,13 @@ namespace RESTTemplate.Controllers
             _logger = logger;
         }
 
+        /*
+         * ----------------------------
+         * HTTP GET (READ) SUM NUMBERS
+         * ---------------------------
+        */
         [HttpGet("sum/{firstNumber}/{secondNumber}")] // Rota que vai ser chamada do request GET da nossa API REST
-        public IActionResult Get(string firstNumber, string secondNumber) // vai pegar os dois atributos passados pelo parâmetros
+        public IActionResult Sum(string firstNumber, string secondNumber) // vai pegar os dois atributos passados pelo parâmetros
         {
             // Lógica de soma de dois números entrados como parâmetros na request
             if (IsNumeric(firstNumber) && IsNumeric(secondNumber)) // valida se são dois valores numéricos
@@ -25,6 +30,65 @@ namespace RESTTemplate.Controllers
 
             return BadRequest("Error! Invalid Input"); // Caso de um 400 BAD REQUEST ele mostra essa mensagem
         }
+
+        /*
+         * -----------------------------------
+         * HTTP GET (READ) SUBTRACTION NUMBERS
+         * -----------------------------------
+        */
+        [HttpGet("sub/{firstNumber}/{secondNumber}")] // Rota que vai ser chamada do request GET da nossa API REST
+        public IActionResult Sub(string firstNumber, string secondNumber) // vai pegar os dois atributos passados pelo parâmetros
+        {
+            // Lógica de soma de dois números entrados como parâmetros na request
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber)) // valida se são dois valores numéricos
+            {
+                var sub = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(sub.ToString()); // Envia um OK 200 e o valor somado
+            }
+
+            return BadRequest("Error! Invalid Input"); // Caso de um 400 BAD REQUEST ele mostra essa mensagem
+        }
+
+        /*
+         * ----------------------------------------
+         * HTTP GET (READ) MULTIPLICATIONS NUMBERS
+         * ----------------------------------------
+        */
+        [HttpGet("mult/{firstNumber}/{secondNumber}")]
+        public IActionResult Mult(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var mult = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(mult.ToString());
+            }
+
+            return BadRequest("Error! Invalid Input");
+        }
+
+        /*
+         * --------------------------------
+         * HTTP GET (READ) DIVISION NUMBERS
+         * --------------------------------
+        */
+        [HttpGet("div/{firstNumber}/{secondNumber}")]
+        public IActionResult Div(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var div = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(div.ToString());
+            }
+
+            return BadRequest("Error! Invalid Input");
+        }
+
+
+        /*
+         * -------------------
+         * MÉTODOS AUXILIARES
+         * -------------------
+         */
 
         private bool IsNumeric(string strNumber)
         {
